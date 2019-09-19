b934c8077625df75042d3226347332320657ef2d
Ray Villalobos <ray@planetoftheweb.com>
2019-09-18 18:21:54 -0700

    01_03 Multiple tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,28 @@
 const {src, dest} = require('gulp');
 
+const origin = 'src';
+const destination = 'build';
+
 function html(cb) {
-  src('src/index.html').pipe(dest('build'));
+  src(`${origin}/**/*.html`).pipe(dest(destination));
   cb();
 }
 
-exports.default = html;
+function css(cb) {
+  src(`${origin}/css/**/*.css`).pipe(dest(`${destination}/css`));
+  cb();
+}
+
+function js(cb) {
+  src([
+    `${origin}/js/lib/bootstrap.bundle.min.js`,
+    `${origin}/js/lib/fontawesome-all.min.js`,
+    `${origin}/js/lib/jquery.min.js`,
+    `${origin}/js/script.js`
+  ]).pipe(dest(`${destination}/js`));
+  cb();
+}
+
+exports.html = html;
+exports.css = css;
+exports.js = js;
